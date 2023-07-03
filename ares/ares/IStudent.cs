549421a7220593ac4575cfd53e0f82d3884f8794
Chirtop Catalin <catalinioan.chirtop@ulbsibiu.ro>
2023-07-03 12:42:25 +0300

    initial commit

--- ares/ares/IStudent.cs
+++ ares/ares/IStudent.cs
@@ -4,3 +4,18 @@ public interface IStudent
 {
     public void SayMyName();
 }
+
+class Student : IStudent
+{
+    private string name;
+
+    public void Student(string name)
+    {
+        this.name = name;
+    }
+    
+    public void SayMyName() 
+    {
+        Console.WriteLine(this.name);
+    }
+}
--- ares/ares/Program.cs
+++ ares/ares/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using ares;
+
 Console.WriteLine("Entry Point");
+string name = "Catalin";
+Student a = new Student(name);
+a.SayMyName();
