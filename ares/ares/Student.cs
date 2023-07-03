9b26ecc00fa64ea27d8d97b87d5b9cf0b4658aad
Bucur Dan-Alexandru <bucur.alexandru.dan@gmail.com>
2023-07-03 12:50:56 +0300

    De forma

--- ares/ares/Student.cs
+++ ares/ares/Student.cs
@@ -4,6 +4,7 @@ public class Student:IStudent
 {
     public void SayMyName()
     {
+        //Comentariu de forma
         string name = "";
         name = Console.ReadLine();
         Console.WriteLine($"My name is {name}");
