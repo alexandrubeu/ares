e03c2c6551c3fea8871fb959ccf7ded1296e962d
Bucur Dan-Alexandru <bucur.alexandru.dan@gmail.com>
2023-07-03 12:18:18 +0300

    Implemented SayMyName Method

---
+++ ares/ares/Student.cs
@@ -0,0 +1,11 @@
+﻿namespace ares;
+
+public class Student:IStudent
+{
+    public void SayMyName()
+    {
+        string name = "";
+        name = Console.ReadLine();
+        Console.WriteLine($"My name is {name}");
+    }
+}
